e707cd81b55aa155fa7452ed53b89ecb76960bad
Dan van der Ster <daniel.vanderster@cern.ch>
2013-11-26 10:14:25 +0100

    rgw fixes

--- code/manifests/yum/ceph.pp
+++ code/manifests/yum/ceph.pp
@@ -11,4 +11,26 @@ class ceph::yum::ceph (
     priority => 5,
     before   => Package['ceph'],
   }
+
+  yumrepo { 'httpd-ceph':
+    descr    => "Apache packages for Ceph",
+    baseurl  => "http://gitbuilder.ceph.com/apache2-rpm-rhel6-x86_64-basic/ref/master",
+    enabled  => 1,
+    priority => 2,
+    gpgcheck => 1,
+    gpgkey   =>
+      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc',
+    before   => Package['mod_ssl'],
+  }
+
+  yumrepo { 'fastcgi-ceph':
+    descr    => "FastCGI packages for Ceph",
+    baseurl  => "http://gitbuilder.ceph.com/mod_fastcgi-rpm-rhel6-x86_64-basic/ref/master",
+    enabled  => 1,
+    priority => 2,
+    gpgcheck => 1,
+    gpgkey   =>
+      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc',
+    before   => Package['mod_fastcgi'],
+  }
 }
