44a996202e74f2288e42224bf7911457f746f369
Dan van der Ster <daniel.vanderster@cern.ch>
2014-10-16 10:04:15 +0200

    Move rgw repos to separate class

--- code/manifests/yum/ceph.pp
+++ code/manifests/yum/ceph.pp
@@ -11,24 +11,4 @@ class ceph::yum::ceph (
     priority => 5,
     before   => Package['ceph'],
   }
-
-  yumrepo { 'httpd-ceph':
-    descr    => "Apache packages for Ceph",
-    baseurl  => "http://gitbuilder.ceph.com/apache2-rpm-rhel6-x86_64-basic/ref/master",
-    enabled  => 1,
-    priority => 2,
-    gpgcheck => 1,
-    gpgkey   =>
-      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc',
-  }
-
-  yumrepo { 'fastcgi-ceph':
-    descr    => "FastCGI packages for Ceph",
-    baseurl  => "http://gitbuilder.ceph.com/mod_fastcgi-rpm-rhel6-x86_64-basic/ref/master",
-    enabled  => 1,
-    priority => 2,
-    gpgcheck => 1,
-    gpgkey   =>
-      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc',
-  }
 }
