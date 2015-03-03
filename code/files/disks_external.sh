1dfb29036e2921b6fc618d3f3a89f5826307903b
Herve Rousseau <hroussea@cern.ch>
2015-03-03 18:21:17 +0100

    Handle duplicates in disks_external fact

--- code/files/disks_external.sh
+++ code/files/disks_external.sh
@@ -10,4 +10,4 @@ echo -n disks_external=
 for ENCLO in `lsscsi -g | grep enclos | sed -r 's/^\[([0-9]+):.*$/\1/'`
 do
   lsscsi -t $ENCLO | grep /dev/sd | awk '{print $4}' | sed 's^/dev/^^'
-done | xargs echo | sed 's/ /,/g'
+done | sort | uniq | xargs echo | sed 's/ /,/g'
